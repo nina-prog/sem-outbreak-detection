39b455f22980f0ec66bc74adeae6e58b4ca6d7f3
nina-m-m <65343815+nina-prog@users.noreply.github.com>
2025-01-16 12:45:12 +0100

    Implement loading configs

---
+++ src/utils.py
@@ -0,0 +1,27 @@
+""" This module contains utility functions for the project. """
+
+import yaml
+from src.logger import setup_logger
+
+logger = setup_logger(__name__, level="INFO")  # Change the level to 'DEBUG' to
+
+
+def load_config(config_path: str) -> dict:
+    """
+    Loads a YAML configuration file.
+
+    :param config_path: Path to the configuration file
+    :type config_path: str
+
+    :return: Configuration dictionary
+    :rtype: dict
+    """
+    try:
+        with open(config_path, "r") as ymlfile:
+            return yaml.load(ymlfile, yaml.FullLoader)
+    except FileNotFoundError:
+        raise FileNotFoundError(f"File {config_path} not found!")
+    except PermissionError:
+        raise PermissionError(f"Insufficient permission to read {config_path}!")
+    except IsADirectoryError:
+        raise IsADirectoryError(f"{config_path} is a directory!")
